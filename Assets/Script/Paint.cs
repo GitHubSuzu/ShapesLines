838a8a6171829637dfb5ff5177d360af19c67a2f
RyoSasakiryo <suzu.engineer@gmail.com>
2022-01-12 11:53:08 +0900

    修正

--- Assets/Script/Paint.cs
+++ Assets/Script/Paint.cs
@@ -7,11 +7,14 @@ using UnityEngine;
 /// </summary>
 public class Paint : MonoBehaviour
 {
-    [SerializeField] private Transform _paintObjParent;
+    [SerializeField]
+    private Transform _paintObjParent;
 
-    [SerializeField] private GameObject _paintObjPrefab;
+    [SerializeField]
+    private GameObject _paintObjPrefab;
 
-    [SerializeField] private GameObject _vertCube;
+    [SerializeField]
+    private GameObject _vertCube;
 
     private GameObject _tmpPaintObj;
     bool check = true;
@@ -19,13 +22,14 @@ public class Paint : MonoBehaviour
     private void Update()
     {
         //スクリーン座標をワールド座標に変換
-        Vector3 worldClickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition +  Camera.main.transform.forward);
+        Vector3 worldClickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + Camera.main.transform.forward);
 
         //マウス押した瞬間
         if (Input.GetMouseButtonDown(0))
         {
             _tmpPaintObj = Instantiate(_paintObjPrefab, worldClickPos, Quaternion.identity);
             _tmpPaintObj.transform.parent = _paintObjParent;
+            check = true;
         }
 
         //マウス押し続けている間
@@ -39,7 +43,7 @@ public class Paint : MonoBehaviour
     /// 全てのTrailRendererの全頂点を取得 Buttonに登録
     /// </summary>
     public void GetVert()
-    {             
+    {
         foreach (Transform child in _paintObjParent.transform)
         {
             TrailRenderer tr = child.GetComponent<TrailRenderer>();
@@ -47,7 +51,7 @@ public class Paint : MonoBehaviour
             Vector3[] posArray = new Vector3[posCount];
 
             //全ての頂点を取ってくる
-            int vertCount =  tr.GetPositions(posArray);
+            int vertCount = tr.GetPositions(posArray);
 
             //描画した頂点座標を確認
             for (int i = 0; i < vertCount; i++)
@@ -56,10 +60,38 @@ public class Paint : MonoBehaviour
                 Instantiate(_vertCube, posArray[i], Quaternion.identity);
             }
 
-            if (check)
+            string shapes = "Triangles";
+            switch (shapes)
             {
-                Triangles(posArray[0], posArray[1], posArray[2]);
-                check = false;
+               case "Triangles":
+                   if (check && vertCount == 3)
+                   {
+                       Debug.Log(vertCount);
+                       Triangles(posArray[0], posArray[1], posArray[2]);
+                       check = false;
+                   }
+                   break;
+               case "Squares":
+                   if (check && vertCount == 4)
+                   {
+                       Squares(posArray[0], posArray[1], posArray[2], posArray[3], "Squares");
+                       check = false;
+                   }
+                   break;
+               case "Pentagon":
+                   if (check && vertCount == 5)
+                   {
+                       Pentagon(posArray[0], posArray[1], posArray[2], posArray[3], posArray[4]);
+                       check = false;
+                   }
+                   break;
+               case "Hexagon":
+                   if (check && vertCount == 6)
+                   {
+                       Hexagon(posArray[0], posArray[1], posArray[2], posArray[3], posArray[4], posArray[5]);
+                       check = false;
+                   }
+                   break;
             }
         }
     }
@@ -70,22 +102,51 @@ public class Paint : MonoBehaviour
         var angleA = Vector3.Angle(b - a, c - a);
         var angleB = Vector3.Angle(a - b, c - b);
         var angleC = Vector3.Angle(b - c, a - c);
-
-        Debug.Log(angleA);
-        Debug.Log(angleB);
-        Debug.Log(angleC);
-
-        Debug.Log(angleA + angleB + angleC);
+        
+        var allAngle = angleA + angleB + angleC;
+        
+        Debug.Log((int) allAngle == 180 ? "正解" : "不正解");
     }
 
     //四角形
-    void Squares(Vector3 a, Vector3 b, Vector3 c, Vector3 d)
+    void Squares(Vector3 a, Vector3 b, Vector3 c, Vector3 d, string shapes)
     {
         var angleA = Vector3.Angle(b - a, d - a);
         var angleB = Vector3.Angle(a - b, c - b);
         var angleC = Vector3.Angle(b - c, d - c);
         var angleD = Vector3.Angle(c - d, a - d);
+        
+        var allAngle = angleA + angleB + angleC + angleD;
+
+        switch (shapes)
+        {
+            case "Squares":
+                Debug.Log((int) allAngle == 360 ? "正解" : "不正解");
+                break;
+            case "rhombus": //ひし形
+                Debug.Log((int) allAngle == 360 && Difference(angleA, angleC) && Difference(angleB, angleD)? "正解" : "不正解");
+                break;
+            case "Trapezoid": //台形
+                Debug.Log((int) allAngle == 360 && Difference(a.y, d.y) && Difference(b.y, c.y) ? "正解" : "不正解");
+                break;
+        }
     }
+
+    bool Difference(float a, float b)
+    {
+        if (a > 0 && b < 0 || a < 0 && b > 0 ) return false;
+        
+        var ans = Mathf.Abs(a - b);
+        if (ans < 5 && ans > -5)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
 
     //五角形
     void Pentagon(Vector3 a, Vector3 b, Vector3 c, Vector3 d, Vector3 e)
@@ -95,6 +156,10 @@ public class Paint : MonoBehaviour
         var angleC = Vector3.Angle(b - c, d - c);
         var angleD = Vector3.Angle(c - d, e - d);
         var angleE = Vector3.Angle(d - e, a - e);
+
+        var allAngle = angleA + angleB + angleC + angleD + angleE;
+        
+        Debug.Log((int) allAngle == 540 ? "正解" : "不正解");
     }
 
     //六角形
@@ -106,24 +171,10 @@ public class Paint : MonoBehaviour
         var angleD = Vector3.Angle(c - d, e - d);
         var angleE = Vector3.Angle(d - e, f - e);
         var angleF = Vector3.Angle(e - f, a - f);
-    }
 
-    //ひし形
-    void rhombus(Vector3 a, Vector3 b, Vector3 c, Vector3 d)
-    {
-        var angleA = Vector3.Angle(b - a, d - a);
-        var angleB = Vector3.Angle(a - b, c - b);
-        var angleC = Vector3.Angle(b - c, d - c);
-        var angleD = Vector3.Angle(c - d, a - d);
-    }
+        var allAngle = angleA + angleB + angleC + angleD + angleE + angleF;
 
-    //台形
-    void Trapezoid(Vector3 a, Vector3 b, Vector3 c, Vector3 d)
-    {
-        var angleA = Vector3.Angle(b - a, d - a);
-        var angleB = Vector3.Angle(a - b, c - b);
-        var angleC = Vector3.Angle(b - c, d - c);
-        var angleD = Vector3.Angle(c - d, a - d);
+        Debug.Log((int) allAngle == 720 ? "正解" : "不正解");
     }
     
 }
